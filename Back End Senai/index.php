7263f1932d8031f421fbc7cb4c3efc95ed7c6427
Rarysson Ferreira <raryssonferreiracandido@gmail.com>
2025-02-27 17:03:39 -0300

    Update index.php

--- Back End Senai/index.php
+++ Back End Senai/index.php
@@ -7,5 +7,18 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+
+  <?php
+  // COMANDOS NÃO SÃO CASE SENSITIVAS
+   echo "Rarysson Ferreira <br>";
+   echo "<br>"; // Fixed the empty echo line
+  
+   $palavra = "banana";
+   echo $palavra;  // Corrected variable name
+   // echo $PALAVRA; // This would cause an error, it's not defined
+  
+   phpinfo(); // Outputs PHP configuration information
+  ?>
+
 </body>
 </html>
